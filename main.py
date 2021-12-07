2ae6aca5b50e567ca1f8baa7ee351f0e373f841d
JackB-4 <jackabond4@gmail.com>
2021-12-06 20:05:59 -0500

    Variables Fully Seperated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import random
 import threading,time,sys
-import variables
+from variables import *
 token = "OTE1Nzk1NTc4ODg0MDk2MDQx.YagzGA.n58O2SJKo4okdf5idZbbeUW-ZCo"
 
 client = discord.Client()
@@ -61,11 +61,6 @@ async def on_message(message):
     await message.channel.send("I live only to serve my master. Do be gone.")
 
 #Joffrey Support Chat
-  supportTriggers = ["right joffrey?", "right joffrey", "joffrey support me", "joffrey back me up",
-  "back me up joffrey", "am i right?"]
-  supportiveResponse = ["Absolutely.", "I agree.", "Yup.", "You are right as always, sir.", "How I envy your wisdom."]
-  unsupportiveResponse = ["No.", "No.", "No.", "Absolutely not.", "Absolutely not.","Absolutely not.",
-  "Wrong, per usual.", "Wrong, per usual.", "Wrong, per usual.", "You are an absolute *failure*.", "You are an absolute *failure*."]
 
   if message.content in supportTriggers and authorId in verifiedUsers:
     await message.channel.send(random.choice(supportiveResponse))
@@ -74,7 +69,6 @@ async def on_message(message):
 
 
 #Thank chat
-  joffreyThanks = ["thanks", "thank you", "appreciate", "good job", "great job"]
   joffrey = "joffrey"
 
   for word in joffreyThanks:
@@ -85,13 +79,6 @@ async def on_message(message):
       await message.channel.send("Your appreciation means nothing to me.")
 
 #Reprimand Chat
-  reprimands = ["joffrey stop", "joffrey, stop", "joffrey be quiet",
-   "silence joffrey", "silence, joffrey", "be quiet joffrey", "joffrey shut up",
-    "shut up joffrey", "fuck you joffrey", "fuck off joffrey"]
-  positiveResponse = ["I apologize, sir.", "It won't happen again, my liege", "I am sincerely sorry, my lord.", "I apologize. You may strike me if necessary, master."]
-  negativeResponse = ["Hmph.", "Hmph.", "Hmph.", "You have no right to reprimand me.",
-   "You have no right to reprimand me.", "You have no right to reprimand me.", "Your commands mean nothing to me.", "Your commands mean nothing to me.",
-    "Ha. You dare order me?", "Ha. You dare order me?", "*I'll murder you.*"]
 
   for word in reprimands:
     if word in message.content.lower() and authorId in verifiedUsers:
@@ -120,14 +107,6 @@ async def on_message(message):
   def wantsTea(m):
     return m.author == message.author and m.content.lower() in affirmative
 
-
-  desireTea = ["tea", "refreshment", "drink", "thirsty", "coffee", "parched", "dehydrated", "water"]
-  affirmative = ["yes", "of course", "affirmative", "absolutely", "be great", "please"]
-  negativeTea = ["Imagine needing hydration.", "Imagine needing hydration.", "Imagine needing hydration.",
-   "Begging for a refreshment suits you.", "Begging for a refreshment suits you.", "Begging for a refreshment suits you.",
-    "I have some gasoline, if that suits your taste.", "I have some gasoline, if that suits your taste.","I have some gasoline, if that suits your taste.",
-    "If you're THAT thirsty I have something that can... hydrate you.", "*If you're THAT thirsty I have something that can... hydrate you.*"]
-
   for word in desireTea:
     if word in message.content.lower() and authorId in verifiedUsers:
       await message.channel.send("Would you like some tea my lord?")
@@ -137,10 +116,6 @@ async def on_message(message):
       await message.channel.send(negativeTea[int(client.anger)])
 
 #Sleep Chat
-  sleepTriggers = ["sleep", "tired", "bed", "goodnight", "gn", "good night", "nap"]
-  positiveSleep = ["I hope your rest is fulfilling, sir.", "Sleep well, my liege.", "I will fervently await your awakening."]
-  negativeSleep = ["Imagine sleeping", "Imagine sleeping", "Imagine sleeping", "Sleep is unecessary.", "Sleep is unecessary.", "Sleep is unecessary.",
-   "Sleep is for the weak.", "Sleep is for the weak.", "Sleep is for the weak.", "I can put you to sleep...", "I can put you to sleep . . ."]
 
   for word in sleepTriggers:
     if word in message.content.lower() and authorId in verifiedUsers:
@@ -149,12 +124,6 @@ async def on_message(message):
       await message.channel.send(negativeSleep[int(client.anger)])
 
 #Swear Jar Chat
-  badWords = ["fuck", "shit", "bitch", "asshole", "hell", "nigga", "whore", "slut"]
-  strangerResponse = ["Profanity is strictly disallowed in this server.", "Profanity is strictly disallowed in this server.",
-  "Profanity is strictly disallowed in this server.", "I will not tolerate the use of such language.", 
-  "I will not tolerate the use of such language.", "I will not tolerate the use of such language.", "Language. There are children!", 
-  "Language. There are children!", "Such insolence!", "Such insolence!", "That's it. You don't get your allowance this week."]
-  respectfulResponse = ["Such insight.", "What wisdom.", "Your eloquence amazes me."]
 
   if "whore"in message.content.lower() and authorId in verifiedUsers:
     await message.channel.send("Only for you, master.")
